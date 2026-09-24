d04e9524357d044e22a4cf71db82390f46ad927e
ïfti020 <iftihaque1@gmail.com>
2026-09-24 12:31:09 +0600

    Add home page explore books section

--- fullstack-library-app/REACT-LIBRARY-FRONTEND/react-library-frontend/src/App.tsx
+++ fullstack-library-app/REACT-LIBRARY-FRONTEND/react-library-frontend/src/App.tsx
@@ -1,13 +1,18 @@
-
 import './App.css'
 import {NavigationBar} from "./layouts/navigation-bars/NavigationBar"
+import {ExploreTopBooks} from "./layouts/home-page/ExploreTopBooks";
 
 function App() {
 
 
   return (
-    <NavigationBar/>
-  )
+      <>
+
+      <NavigationBar/>
+      <ExploreTopBooks/>
+
+      </>
+  );
 }
 
 export default App
